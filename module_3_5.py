08bd34514388e047a45f8be00aa2a53d4b118be0
6u6lik16 <serega22222223@mail.ru>
2024-10-07 03:47:15 +0400

    Add files via upload

---
+++ module_3_5.py
@@ -0,0 +1,14 @@
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first=int(str_number[0])
+    if first == 0:
+        return 1
+    else:
+        if len(str_number)>1:
+            return first * get_multiplied_digits(int(str_number[1:]))
+        else:
+            return first
+
+ishod="00402030000"
+result = get_multiplied_digits(int(ishod))
+print(result)
